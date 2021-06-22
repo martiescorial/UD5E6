7fbd8fcec9f8548bad34b285162754bcefbd416d
Martina <martinaescorial9@gmail.com>
2021-06-22 16:04:41 +0200

    Add project files.

---
+++ UD5E6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace UD5E6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            do
+            {
+                String texto = "Introduce un numero positivo";
+                numero = Convert.ToInt32(texto);
+            } while (numero > 0);
+            int numCifras = cuentaCifras(numero);
+           
+            if (numCifras == 1)
+            {
+                Console.WriteLine("El numero " + numero + " tiene " + numCifras + " cifra");
+            }
+            else
+            {
+                Console.WriteLine("El numero " + numero + " tiene " + numCifras + " cifras");
+            }
+        }
+        public static int cuentaCifras(int numero)
+        {
+            int contador = 0;
+            for (int i = numero; i > 0; i /= 10)
+            {
+               
+                contador++;
+            }
+            return contador;
+            
+        }
+    }
+}
